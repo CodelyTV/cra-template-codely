6515f3fdeea4047aba7719b70dbe7dcd494ab9c4
Isma Navarro <ismanapa@gmail.com>
2022-10-06 16:27:11 +0200

    refactor: transform ts files to react components

---
+++ template/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p>🌱⚛️ Create React App Codely template example</p>
+    </div>
+  );
+}
+
+export default App;
