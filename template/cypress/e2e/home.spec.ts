c5d4b5b8db304a12401f0f69607cf1bdf85556a8
Isma Navarro <ismanapa@gmail.com>
2022-10-11 18:52:14 +0200

    feat: configure linter and stylelint

--- template/cypress/e2e/home.spec.ts
+++ template/cypress/e2e/home.spec.ts
@@ -1,6 +1,8 @@
 describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("/");
-    cy.findByRole("heading", { name: /🌱⚛️ Create React App Codely template example/i }).should("exist");
+    cy.findByRole("heading", { name: /🌱⚛️ Create React App Codely template example/i }).should(
+      "exist"
+    );
   });
 });
--- template/cypress/support/e2e.ts
+++ template/cypress/support/e2e.ts
@@ -14,5 +14,4 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
-
+import "./commands"
